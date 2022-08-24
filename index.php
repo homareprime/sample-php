336f2b4d4eb60549b75c09785a1ac6605b9b78b0
homareprime <hwatanab@redhat.com>
2022-08-24 11:37:39 +0900

    Code altered

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
-  echo "<h1>Hello OpenShift World!</h1>\n"; 
+  echo "<h1>YOKOSO OpenShift World!</h1>\n"; 
   echo "hostname: " . gethostname() . "<BR>\n";
   echo "IP-Addr : " . $_SERVER['SERVER_ADDR'] . "\n"; ;?>
